25972a8207188adb50f976622839bc5a98031158
guptahardik <38167695+guptahardik@users.noreply.github.com>
2020-07-21 19:39:06 -0700

    Add files via upload

---
+++ LoopChallenge/LoopChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var count = 0;
+            while (count < 5) { 
+                Console.WriteLine("This is start of loop no. " + (count + 1)+ "\n");
+                for (int i = 1; i < 11; i++)
+                {
+                    Console.WriteLine(i);
+
+                }
+                for (int i = 10; i >= 1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+                count++;
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
