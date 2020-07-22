122df4f68dbb54cee2cca6d5e105a0ebcf4a16a6
guptahardik <38167695+guptahardik@users.noreply.github.com>
2020-07-21 19:30:26 -0700

    Add files via upload

---
+++ SurveyOfUser/README.md
@@ -0,0 +1,2 @@
+# CSharpSampleApplication
+This is a sample C# application where with the help of Console Input, Console Output, Variable usage and String Concatenation the users gets an output message based on user's input.
---
+++ SurveyOfUser/SampleSurveyConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SampleSurveyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please fill the survey. Thanks!!\n");
+
+            Console.WriteLine("What is the passcode you were given?");
+            var pass = Console.ReadLine();
+            if(pass == "secret")
+            {
+                Console.WriteLine("Access Granted\n");
+                Console.WriteLine("What is your name?");
+                var name = Console.ReadLine();
+                Console.WriteLine("How old are you?");
+                var age = Console.ReadLine();
+                Console.WriteLine("What month were you born in?");
+                var month = Console.ReadLine();
+
+                Console.WriteLine("You are " + name + " and your age is " + age + " years old. Also you were born in " + month + ".");
+            }
+            else
+            {
+                Console.WriteLine("Access Denied");
+                
+            }
+            
+        }
+    }
+}
